f21eecee22319f920278d637c152b837a6ca601e
lendZ1 <krognes.erlend@gmail.com>
2026-02-07 20:20:43 +0100

    fjernet print(hei) loopen

--- audio/audio_manager.py
+++ audio/audio_manager.py
@@ -1,14 +1,15 @@
 import pygame
 
+
 class AudioManager:
     def __init__(self):
         pygame.mixer.init()
         self.music_volume = 0.5
 
-    def play_music(self, path, loop = True, fade_ms=1000): 
+    def play_music(self, path, loop=True, fade_ms=1000):
         pygame.mixer.music.load(path)
-        pygame.mixer.music.set_volume(self.music_volume) 
-        pygame.mixer.music.play(-1 if loop else 0, fade_ms=fade_ms)     
+        pygame.mixer.music.set_volume(self.music_volume)
+        pygame.mixer.music.play(-1 if loop else 0, fade_ms=fade_ms)
 
     def stop_music(self, fade_ms=1000):
         pygame.mixer.music.fadeout(fade_ms)
--- main.py
+++ main.py
@@ -13,11 +13,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-var = 0
-while var != 10:
-    var += 1
-    print("hei")
-
     # --- Lyd --- test
 
 # import os
