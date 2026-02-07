e3871ed69495653de38d1339fe4c28356c3c1045
lendZ1 <krognes.erlend@gmail.com>
2026-02-07 17:40:23 +0100

    nodene har dimensjonene på hele grafen

--- src/node.py
+++ src/node.py
@@ -15,28 +15,25 @@ class Node:
         self.rightTopNeighbor
         self.rightBottomNeighbor
 
-    def createRightNeighbor(self, xSize, ySize, leftNeighbor):
+    def createRightNeighbor(self,leftNeighbor):
         self.rightNeighbor=Node(self.xPos+1, self.yPos, self.xSize, self.ySize, not self.Alive)
         self.rightNeighbor.leftNeighbor=leftNeighbor
 
-        if ySize<=0: #Recursively creates nodes on X axis
-            self.rightNeighbor.createRightNeighbor(xSize-1, ySize, self)
+        if self.xSize-self.xPos<=0: #Recursively creates nodes on X axis
+            self.rightNeighbor.createRightNeighbor(self)
 
-        self.rightNeighbor.createBottomNeighbor(ySize-1, self.leftNeighbor, self.rightNeighbor, self)
+        self.rightNeighbor.createBottomNeighbor(self.leftNeighbor, self.rightNeighbor, self)
 
 
-    def createBottomNeighbor(self, ySize, leftNeighbor, rightNeighbor, topNeighbor):
+    def createBottomNeighbor(self, leftNeighbor, rightNeighbor, topNeighbor):
         self.bottomNeighbor=Node(self.xPos, self.yPos-1, not self.Alive)
 
         self.bottomNeighbor.leftNeighbor=leftNeighbor
         self.bottomNeighbor.rightNeighbor=rightNeighbor
         self.bottomNeighbor.topNeighbor=topNeighbor
 
-
-        
-
-        if ySize<=0: #Recursively creates nodes on Y axis
-            self.bottomNeighbor.createBottomNeighbor(ySize-1)
+        if self.ySize-self.yPos <=0: #Recursively creates nodes on Y axis
+            self.bottomNeighbor.createBottomNeighbor(self.leftNieghbor.bottomNeighbor, self.rightNeighbor.bottomNeighbor, self)
 
         
 
