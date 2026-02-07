6ed45612ca3c91c9611177805f034f23ba18b173
lendZ1 <krognes.erlend@gmail.com>
2026-02-07 16:40:20 +0100

    fikset funksjons argumenter

--- src/node.py
+++ src/node.py
@@ -1,7 +1,9 @@
 class Node:
-    def __init__(self, xPos, yPos, Alive):
+    def __init__(self, xPos, yPos, xSize, ySize, Alive):
         self.xPos=xPos
         self.yPos=yPos
+        self.xSize=xSize
+        self.ySize=ySize
         self.isAlive=Alive
 
         self.topNeighbor
@@ -14,20 +16,24 @@ class Node:
         self.rightBottomNeighbor
 
     def createRightNeighbor(self, xSize, ySize, leftNeighbor):
-        self.rightNeighbor=Node(self.xPos+1, self.yPos, not self.Alive, self)
+        self.rightNeighbor=Node(self.xPos+1, self.yPos, self.xSize, self.ySize, not self.Alive)
         self.rightNeighbor.leftNeighbor=leftNeighbor
 
         if ySize<=0: #Recursively creates nodes on X axis
-            self.rightNeighbor.createRightNeighbor(xSize-1, ySize)
+            self.rightNeighbor.createRightNeighbor(xSize-1, ySize, self)
 
         self.rightNeighbor.createBottomNeighbor(ySize-1, self.leftNeighbor, self.rightNeighbor, self)
 
 
     def createBottomNeighbor(self, ySize, leftNeighbor, rightNeighbor, topNeighbor):
         self.bottomNeighbor=Node(self.xPos, self.yPos-1, not self.Alive)
+
         self.bottomNeighbor.leftNeighbor=leftNeighbor
         self.bottomNeighbor.rightNeighbor=rightNeighbor
         self.bottomNeighbor.topNeighbor=topNeighbor
+
+
+        
 
         if ySize<=0: #Recursively creates nodes on Y axis
             self.bottomNeighbor.createBottomNeighbor(ySize-1)
