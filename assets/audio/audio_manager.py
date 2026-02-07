641776a15b6697f62e8280c218f08359b43799a5
Thananthan Kirupakaran <baasshabhai1810@gmai.com>
2026-02-07 19:43:27 +0100

    Fullfort Audio manager v_1

--- assets/audio/audio_manager.py
+++ assets/audio/audio_manager.py
@@ -3,4 +3,16 @@ import pygame
 class AudioManager:
     def __init__(self):
         pygame.mixer.init()
-        self.   
+        self.music_volume = 0.5
+
+    def play_music(self, path, loop = True, fade_ms=1000): 
+        pygame.mixer.music.load(path)
+        pygame.mixer.music.set_volume(self.music_volume) 
+        pygame.mixer.music.play(-1 if loop else 0, fade_ms=fade_ms)     
+
+    def stop_music(self, fade_ms=1000):
+        pygame.mixer.music.fadeout(fade_ms)
+
+    def set_music_volume(self, volume):
+        self.music_volume = max(0.0, min(1.0, volume))  # Clamp between 0.0 and 1.0
+        pygame.mixer.music.set_volume(self.music_volume)
